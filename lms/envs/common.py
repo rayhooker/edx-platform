b19db2b7909e30d39bb96fc4cb2ae67ff3753491
Bertrand Marron <bertrand.marron@ionis-group.com>
2014-03-17 16:45:15 +0100

    Add XForwardedFor middleware

--- lms/envs/common.py
+++ lms/envs/common.py
@@ -711,6 +711,9 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.common.CommonMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
 
+    # Reverse proxy
+    'x_forwarded_for.middleware.XForwardedForMiddleware',
+
     # Instead of AuthenticationMiddleware, we use a cached backed version
     #'django.contrib.auth.middleware.AuthenticationMiddleware',
     'cache_toolbox.middleware.CacheBackedAuthenticationMiddleware',
@@ -1194,6 +1197,9 @@ INSTALLED_APPS = (
 
     # Monitoring functionality
     'monitoring',
+
+    # Reverse proxy
+    'x_forwarded_for',
 )
 
 ######################### MARKETING SITE ###############################
